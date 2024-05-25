0a96830f8066b7dcaf64a2e4ab977c79a756d4ea
Dmitry Shagurov <shagurovda@gmail.com>
2024-05-25 09:22:34 +0400

    Update Footer

--- src/components/sections/Footer/Footer.tsx
+++ src/components/sections/Footer/Footer.tsx
@@ -1,3 +1,8 @@
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import FacebookIcon from '@mui/icons-material/Facebook';
+
 import { SectionContent } from "components/SectionContent";
 import { CAFE_NAME } from "constants/cafe";
 
@@ -8,7 +13,21 @@ const Footer = () => {
   return (
     <footer className={styles.Footer}>
       <SectionContent className={styles.content}>
-        <div>{CAFE_NAME}</div>
+        <div>
+          <p>{CAFE_NAME}©. Все права защищены.</p>
+          <div className={styles.socialMedia}>
+            <a href="https://yandex.com/" target="_blank"><InstagramIcon /></a>
+            <a href="https://yandex.com/" target="_blank"><XIcon /></a>
+            <a href="https://yandex.com/" target="_blank"><YouTubeIcon /></a>
+            <a href="https://yandex.com/" target="_blank"><FacebookIcon /></a>
+          </div>
+          </div>
+        <div>
+          <h3>Юридические контакты</h3>
+          <p>г. Москва, ул. Тверская, д. XX, 125XXX</p>
+          <p>magic-castle@example.ru</p>
+          <p>+7 (908) 404-XXXX</p>
+        </div>
       </SectionContent>
     </footer>
   );
